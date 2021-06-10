b3eb734479914dfda55241825b374122330b2bcb
JordanTanReiYao <44694137+JordanTanReiYao@users.noreply.github.com>
2021-06-10 20:43:43 +0800

    Upload flask integration code and requirements

---
+++ ml/src/ML_Flask.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import numpy as np
+import glob
+import tensorflow as tf
+from keras.models import load_model
+
+CNNmodel = load_model('JordanModel1')
+
+def predict(directory):
+    imageToPredict=glob.glob(directory)
+    imageArray=np.array([np.array(Image.open(imageToPredict).resize((495,426)))])
+    prediction=CNNmodel.predict(imageArray)
+    score = tf.nn.softmax(prediction[0])
+    predictedCat=['Fire','Traffic','Injury','Normal'][np.argmax(score)]
+    return predictedCat
